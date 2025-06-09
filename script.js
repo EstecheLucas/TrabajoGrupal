f90eb737317e04fbbc87ca9bbd2d6af49d46a713
Alexis <alexispolo@github.com>
2025-06-09 17:06:04 -0300

    feat: :sparkles: trabajo terminado

---
+++ script.js
@@ -0,0 +1,72 @@
+let model;
+const chooseFiles = document.getElementById("chooseFiles");
+const modelNameSelect = document.getElementById("modelNameSelect");
+const segmentImageButton = document.getElementById("segmentImage");
+const legendsDiv = document.getElementById("legends");
+const image = document.getElementById("image");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+document.getElementById("loadModel").onclick = async () => {
+  segmentImageButton.disabled = true;
+  updateModelLoadStatus("Cargando modelo...");
+
+  const modelName =
+    modelNameSelect.options[modelNameSelect.selectedIndex].value;
+  await loadModel(modelName);
+  updateModelLoadStatus("El modelo " + modelName + " esta cargado!");
+
+  segmentImageButton.disabled = false;
+};
+
+async function loadModel(modelName) {
+  model = await deeplab.load({ base: modelName, quantizationBytes: 2 });
+}
+
+function updateModelLoadStatus(status) {
+  document.getElementById("modelLoadedStatus").innerHTML = status;
+}
+
+chooseFiles.onchange = () => {
+  const [file] = chooseFiles.files;
+  if (file) {
+    image.src = URL.createObjectURL(file);
+  }
+};
+
+segmentImageButton.onclick = predict;
+
+async function predict() {
+  let prediction = await model.segment(image);
+  renderPrediction(prediction);
+}
+
+function renderPrediction(prediction) {
+  const { legend, height, width, segmentationMap } = prediction;
+  console.log(`prediction: ${JSON.stringify(prediction.legend)}`);
+
+  const segmentationMapData = new ImageData(segmentationMap, width, height);
+  canvas.width = image.width;
+  canvas.height = image.height;
+  ctx.putImageData(segmentationMapData, 0, 0);
+
+  displayLegends(legend);
+}
+
+function displayLegends(legendObj) {
+  legendsDiv.innerHTML = "";
+  document.getElementById("legendLabel").style.visibility = "visible";
+
+  Object.keys(legendObj).forEach((legend) => {
+    const [red, green, blue] = legendObj[legend];
+
+    const span = document.createElement("span");
+    span.innerHTML = legend;
+    span.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    span.style.padding = "10px";
+    span.style.marginRight = "10px";
+    span.style.color = "#ffffff";
+
+    legendsDiv.appendChild(span);
+  });
+}
